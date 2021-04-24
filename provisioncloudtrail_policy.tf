f6c573e0f5b633ddd5f93d5a2cf2b550ea1c36a9
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-04-23 22:47:45 -0400

    Add necessary permissions

--- provisioncloudtrail_policy.tf
+++ provisioncloudtrail_policy.tf
@@ -8,10 +8,45 @@ data "aws_iam_policy_document" "provisioncloudtrail_doc" {
   # logs are stored.
   statement {
     actions = [
-      "s3:*"
+      "s3:*",
     ]
     resources = [
       "arn:aws:s3:::${var.bucket_prefix}*",
+    ]
+  }
+
+  # Permissions necessary to manipulate the CloudTrail trail that
+  # generates the logs for CDM.
+  statement {
+    actions = [
+      "cloudtrail:*",
+    ]
+    resources = [
+      "arn:aws:cloudtrail:${var.aws_region}:${local.account_id}:trail/${var.trail_name}",
+    ]
+  }
+
+  # Permissions necessary to manipulate the SQS queue that contains
+  # messages indicating CloudTrail logs that have been added to the S3
+  # bucket.
+  statement {
+    actions = [
+      "sqs:*",
+    ]
+    resources = [
+      "arn:aws:sqs:${var.aws_region}:${local.account_id}:${var.queue_name}",
+    ]
+  }
+
+  # Permissions necessary to manipulate the SNS topic that publishes
+  # notifications when CloudTrail logs that have been added to the S3
+  # bucket.
+  statement {
+    actions = [
+      "sns:*",
+    ]
+    resources = [
+      "arn:aws:sns:${var.aws_region}:${local.account_id}:${var.topic_name}",
     ]
   }
 }
