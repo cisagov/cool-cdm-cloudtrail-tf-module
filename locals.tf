4e1d3ee6ca53e6938a6b3b0347bbce15f90a2270
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-04-23 22:37:27 -0400

    Add a local variable for the account ID associated with the main account

--- locals.tf
+++ locals.tf
@@ -1,4 +1,12 @@
 # ------------------------------------------------------------------------------
+# Retrieve the caller identity for the default provider in order to
+# get the account ID associated with the account to which we want to
+# grant CloudTrail log access for CDM.
+# ------------------------------------------------------------------------------
+data "aws_caller_identity" "default" {
+}
+
+# ------------------------------------------------------------------------------
 # Retrieve the caller identity for the "users" provider in order to
 # get the account ID associated with the Users account.
 # ------------------------------------------------------------------------------
@@ -10,6 +18,10 @@ data "aws_caller_identity" "users" {
 # Evaluate expressions for use throughout this configuration.
 # ------------------------------------------------------------------------------
 locals {
+  # The account ID for the account to which we want to grant
+  # CloudTrail log access for CDM
+  account_id = data.aws_caller_identity.default.account_id
+
   # The account ID for the Users account
   users_account_id = data.aws_caller_identity.users.account_id
 }
